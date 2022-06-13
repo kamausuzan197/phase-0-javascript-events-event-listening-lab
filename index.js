975db2cefdebcba9d8d1a254ce56e49d70bd5016
kamausuzan197 <kamaususan197@gmail.com>
2022-06-13 12:29:07 +0300

    doing assignment

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-let input
-
-function addingEventListener(){
-    input = document.querySelector("#input").addEventListener("click", (input))
+//i wrote a function 
+function addingEventListener() {
+    //i created a variable input form html.
+    const input = document.getElementById('input');
+    //i decided  to get the elemnt by id, rather than querySelector.......
+    input.addEventListener('click', clickAlert);
+    //an eventlistener consists of the event and a callback function.
+    function clickAlert() {
+        alert('I was clicked!');
+    }
 }
 
 
